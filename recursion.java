8fb15bc1ae9d93e32e77b4b264f6c199c2daeec6
vsofat <vsofat00@stuy.edu>
2019-02-04 01:22:15 -0500

    started fib

--- recursion.java
+++ recursion.java
@@ -6,19 +6,27 @@ public class recursion{
     return squareRoot(num,1,threshold);
     }
 
-  public static double sqrt(double num){
-    if (n == 0){
+  public static double sqrt(double num){ //overrides previous function
+    if (num == 0){
       return 0;}
-    return squareRoot(num,1,0.00001);
+    return squareRoot(num,1,0.00001); //threshold from documentation on website
   }
 
   public static double squareRoot(double num, double guess, double threshold){ //helper method
     if(num == 0){ //technically don't need this if in the other methods
     return 0;}
-  if((Math.abs((num-g*g) / num) <= threshold)){
-    return g;}
+  if((Math.abs((num-guess*guess) / num) <= threshold)){
+    return guess;}
   else{
     double Newguess=(((num/guess) + guess)/2);
     return squareRoot(num,guess,threshold);}}
+
+  public static int fib(int num){
+    if (num < 2){
+      return 1;
+    }
+    return fibLinear(num, 1, 1);
+  }
+
 
   }
